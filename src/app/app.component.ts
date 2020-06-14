78838c5edc3eceb0037e08a5148f438e0e78e5dd
capulusnoctis <axel.solares.g@gmail.com>
2020-06-14 03:54:32 -0600

    Added list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'seko-store';
+  nombre = 'Axel';
+  items = ['juanito', 'lila', 'axel'];
+
+  addItem(nuevoItem: string) {
+    this.items.push(nuevoItem)
+  }
+  delteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
