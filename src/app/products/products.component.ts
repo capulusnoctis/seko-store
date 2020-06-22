3a9b85cea2b386feb6f37a8f171b1c5845b9f8ff
capulusnoctis <axel.solares.g@gmail.com>
2020-06-21 23:04:50 -0600

    Created service

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,51 +9,6 @@ import { Product } from '../product.model';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'https://bit.ly/3diOklu',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'https://bit.ly/2zKWDZF',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'https://bit.ly/3fAJrWs',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'https://bit.ly/3ei0cFD',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'https://bit.ly/2YPlCDL',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'https://bit.ly/37L4bZ4',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-  ];
-
   clickProduct(id: number) {
     console.log(`Producto: ${id}`);
   }
