99ccbe0b28da9069ca7fa5dd343a147dabbbf0d4
capulusnoctis <axel.solares.g@gmail.com>
2020-06-23 00:50:37 -0600

    Generated: ProductService.

---
+++ src/app/products.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'https://bit.ly/3diOklu',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'https://bit.ly/2zKWDZF',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'https://bit.ly/3fAJrWs',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'https://bit.ly/3ei0cFD',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'https://bit.ly/2YPlCDL',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'https://bit.ly/37L4bZ4',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
+
+  constructor() { }
+
+  getAllProducts() {
+    return this.products;
+  }
+
+}
