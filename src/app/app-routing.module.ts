f3752b36b1d6a7e5b8a68b1ed4181d7f4daaa53c
capulusnoctis <axel.solares.g@gmail.com>
2020-06-28 03:05:21 -0600

    Commit in the middle of making module of products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { ProductsComponent } from './products/products.component';
+import { ProductsComponent } from './products/components/products/products.component';
 import { ContactComponent } from './contact/contact.component';
-import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
@@ -27,18 +25,14 @@ const routes: Routes = [
         component: ProductsComponent
       },
       {
-        path: 'products/:id',
-        component: ProductDetailComponent
-      },
-      {
         path: 'contact',
         component: ContactComponent
+      },
+      {
+        path: 'demo',
+        loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
       }
     ]
-  },
-  {
-    path: 'demo',
-    component: DemoComponent
   },
   {
     path: '**',
