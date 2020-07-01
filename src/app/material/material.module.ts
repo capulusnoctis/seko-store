a7eb44116a8f21279b9b5d8cbe15748cfed8cf8c
capulusnoctis <axel.solares.g@gmail.com>
2020-06-30 23:02:22 -0600

    Installed flexboxgrid

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -10,7 +11,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatButtonModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
