426083f6cf29b516423aba33b579a03a1a2a36fc
capulusnoctis <axel.solares.g@gmail.com>
2020-08-05 00:19:02 -0600

    Created form to submit new products

---
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductsService } from 'src/app/core/services/products/products.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-form-product',
+  templateUrl: './form-product.component.html',
+  styleUrls: ['./form-product.component.scss']
+})
+export class FormProductComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if(this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product)
+      .subscribe((newProduct) => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price: [0, [Validators.required]],
+      image: [''],
+      description: ['', [Validators.required]]
+    });
+  }
+
+}
